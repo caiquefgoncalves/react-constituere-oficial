340d386e8ee18e06b168929c836c39c96a148a79
Caique Fachini Gonçalves <caiquefachinigoncalves@gmail.com>
2026-08-17 17:28:40 -0300

    Add files via upload

---
+++ src/pages/CadastroEscritorio.jsx
@@ -0,0 +1,5 @@
+import CadastroEscritorio1 from "../components/CadastroEscritorio1/CadastroEscritorio1.jsx";
+
+export default function CadastroEscritorio({ api }) {
+    return <CadastroEscritorio1 api={api} />;
+}
