546d2debe55d7e4b2bb32d707c63618a4f222d7f
Caique Fachini Gonçalves <caiquefachinigoncalves@gmail.com>
2026-08-18 17:26:59 -0300

    Add files via upload

---
+++ src/pages/DashboardEscritorio.jsx
@@ -0,0 +1,5 @@
+import DashboardEscritorio1 from "../components/DashboardEscritorio1/DashboardEscritorio1.jsx";
+
+export default function DashboardEscritorio({ api }) {
+    return <DashboardEscritorio1 api={api} />;
+}
