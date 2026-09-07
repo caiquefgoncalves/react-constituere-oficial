d8767a4208b1f1182be3c621b1892729467e95c6
Caique Fachini Gonçalves <caiquefachinigoncalves@gmail.com>
2026-09-07 00:45:51 -0300

    Add files via upload

---
+++ src/pages/PagamentosLista.jsx
@@ -0,0 +1,5 @@
+import PagamentosLista1 from "../components/PagamentosLista1/PagamentosLista1.jsx";
+
+export default function PagamentosLista({ api }) {
+    return <PagamentosLista1 api={api} />;
+}
